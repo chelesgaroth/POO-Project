700e41f5f618bf71ac89e7279260cf88d854a3c4
emadias <ema.dias.2001@gmail.com>
2020-05-14 15:54:33 +0100

    files e readfile

---
+++ src/ReadFiles.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class ReadFiles {
+    public static void main(String args[]){
+       File futilizadores =  new File("Utlizadores.txt");
+      int i=0;
+      try {
+        i=0;
+        FileReader buf = new FileReader(futilizadores);
+        BufferedReader lerBuf = new BufferedReader(buf);
+        String linha = lerBuf.readLine();
+        while (linha != null) {
+            System.out.printf(linha);
+
+            linha = lerBuf.readLine();
+            i++;
+        }
+
+        buf.close();
+    } catch (
+    IOException e) {
+        System.err.printf("Erro na abertura do arquivo dos clientes: %s\n", e.getMessage());
+    }
+        System.out.println("\nFim de leitura do ficheiro " + futilizadores);
+
+
+}}
